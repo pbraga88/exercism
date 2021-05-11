c486d6fb15aa2c63576d364ced891313b8925a7c
Paulo Braga <paulo.braga1388@gmail.com>
2021-05-10 21:45:10 -0300

    sync

---
+++ workspace/cpp/leap/leap.cpp
@@ -0,0 +1,7 @@
+#include "leap.h"
+
+namespace leap {
+	bool is_leap_year(int year) {
+		return (!(year%4) && (year%100) && (year%400)) || (!(year%4) && !(year%100) && !(year%400));
+	}
+}  // namespace leap
---
+++ workspace/cpp/reverse-string/reverse_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "reverse_string.h"
+
+namespace reverse_string {
+	std::string reverse_string(std::string str) {
+		std::reverse(str.begin(), str.end());
+		return str;
+	}
+}  // namespace reverse_string
+ 
